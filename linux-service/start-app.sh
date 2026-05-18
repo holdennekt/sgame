5e2829e71b880e9656feaf6abf02eeb4b7487ef3
holdennekt <ngryshchak@gmail.com>
2026-05-18 19:54:24 +0200

    fix: get secret with curl

--- linux-service/start-app.sh
+++ linux-service/start-app.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-DESEC_TOKEN=$(gcloud secrets versions access latest --secret="DESEC_TOKEN")
+GCP_TOKEN=$(curl -sf "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" \
+  -H "Metadata-Flavor: Google" | python3 -c "import sys,json; print(json.load(sys.stdin)['access_token'])")
+DESEC_TOKEN=$(curl -sf "https://secretmanager.googleapis.com/v1/projects/smart-altar-496311-p4/secrets/DESEC_TOKEN/versions/latest:access" \
+  -H "Authorization: Bearer $GCP_TOKEN" | python3 -c "import sys,json,base64; print(base64.b64decode(json.load(sys.stdin)['payload']['data']).decode())")
 DESEC_DOMAIN="sgame.dedyn.io"
 
 git -C /opt/sgame pull
